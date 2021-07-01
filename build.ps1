b378e40caf5f8e3c90512ea1ea7c543bc69313c4
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2021-07-01 19:36:12 +0200

    feat: add TrendMiner

--- build.ps1
+++ build.ps1
@@ -2,4 +2,4 @@
 # Create a pdf from the html page
 docker run --rm -v ${PWD}:/converted/ arachnysdocker/athenapdf athenapdf index.html resume.pdf
 # Reset the background color using git :-)
-git checkout -- .\assets\css\style.css
+# git checkout -- ./assets/css/style.css
